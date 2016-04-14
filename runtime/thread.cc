5c4b2ec16bcd779731bed1d2a0bd902dba3d5f92
Mathieu Chartier <mathieuc@google.com>
2016-04-14 10:49:19 -0700

    Write conflict tables in image
    Add image sections for runtime methods and conflict tables. This
    makes it that we do not need to fake up a length prefixed array
    for runtime methods.
    
    Reduces .art private dirty and PSS.
    
    System wide .art PSS goes from 32.5MB to 30.5MB after system boot.
    
    Business card .art private dirty goes from 588K to 504K.
    
    Increases image size by ~100K.
    
    Bug: 27906566
    
    (cherry picked from commit cda9386add68d94697449c6cb08b356747e55c21)
    
    Change-Id: I3ce151651bf95047b0f692c185016f693ac4f820

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -3016,7 +3016,6 @@ size_t Thread::NumberOfHeldMutexes() const {
   return count;
 }
 
-
 void Thread::DeoptimizeWithDeoptimizationException(JValue* result) {
   DCHECK_EQ(GetException(), Thread::GetDeoptimizationException());
   ClearException();
@@ -3035,6 +3034,13 @@ void Thread::DeoptimizeWithDeoptimizationException(JValue* result) {
     SetException(pending_exception);
   }
   interpreter::EnterInterpreterFromDeoptimize(this, shadow_frame, from_code, result);
+}
+
+void Thread::SetException(mirror::Throwable* new_exception) {
+  CHECK(new_exception != nullptr);
+  // TODO: DCHECK(!IsExceptionPending());
+  tlsPtr_.exception = new_exception;
+  // LOG(ERROR) << new_exception->Dump();
 }
 
 }  // namespace art
