a06481e0f732595ffb392db6d97641c0ab73ce37
Brian Carlstrom <bdc@google.com>
2016-06-08 23:48:27 -0700

    Bump frame size limit 8 bytes to fix x86_64 build with latest external/libcxx
    Change-Id: Ib8964d18aa509ee40a91d92eea1011447d429ed7

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -347,7 +347,7 @@ ifeq ($(HOST_OS),linux)
       ifdef SANITIZE_TARGET
         art_target_non_debug_cflags += -Wframe-larger-than=6400
       else
-        art_target_non_debug_cflags += -Wframe-larger-than=1728
+        art_target_non_debug_cflags += -Wframe-larger-than=1736
       endif
     endif
   endif
