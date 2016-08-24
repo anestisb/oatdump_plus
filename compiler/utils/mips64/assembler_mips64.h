multi-os-engine/art

MIPS64: Improve non-trivial 64-bit constant loading.
- Add special case where the upper 32 bits of the constant are equal to
  the lower 32 bits of the constant. Use the "dinsu" instruction to
  replicate the bottom 32 bits into the top 32 bits.

- Test output of LoadConst32()/LoadConst64() against various test
  constants.
  o Make the implementation of LoadConst32()/LoadConst64() into
    templates.
  o Using the template code for LoadConst64() implement test
    cases which simulate the computation of the value which the
    generated code sequence is actually supposed to load.
  o Add conditionally compiled code to verify that all of the
    various data paths through the LoadConst64() function
    actually get exercised.

Change-Id: Ie1ec050e65c666168a9ea582bae9fda97e4cf36a

compiler/utils/mips64/assembler_mips64.h
@@ -31,6 +31,350 @@
 namespace art {
 namespace mips64 {
 
+enum LoadConst64Path {
+  kMipsPathZero           = 0x0,
+  kMipsPathOri            = 0x1,
+  kMipsPathDaddiu         = 0x2,
+  kMipsPathLui            = 0x4,
+  kMipsPathLuiOri         = 0x8,
+  kMipsPathOriDahi        = 0x10,
+  kMipsPathOriDati        = 0x20,
+  kMipsPathLuiDahi        = 0x40,
+  kMipsPathLuiDati        = 0x80,
+  kMipsPathDaddiuDsrlX    = 0x100,
+  kMipsPathOriDsllX       = 0x200,
+  kMipsPathDaddiuDsllX    = 0x400,
+  kMipsPathLuiOriDsllX    = 0x800,
+  kMipsPathOriDsllXOri    = 0x1000,
+  kMipsPathDaddiuDsllXOri = 0x2000,
+  kMipsPathOriDahiDati    = 0x4000,
+  kMipsPathDaddiuDahiDati = 0x8000,
+  kMipsPathDaddiuDahi     = 0x10000,
+  kMipsPathDaddiuDati     = 0x20000,
+  kMipsPathLuiOriDahiDati = 0x40000,
+  kMipsPathLuiOriDahi     = 0x80000,
+  kMipsPathLuiOriDati     = 0x100000,
+  kMipsPathLuiDahiDati    = 0x200000,
+  kMipsPathOriDinsu       = 0x400000,
+  kMipsPathDaddiuDinsu    = 0x800000,
+  kMipsPathLuiDinsu       = 0x1000000,
+  kMipsPathLuiOriDinsu    = 0x2000000,
+  kMipsPathAllPaths       = 67108863,
+  kMipsPathDahi           = 0x4000000,  // Not a path per se, but needed in calculating which paths
+  kMipsPathDati           = 0x8000000,  // were covered.
+  kMipsPathAddiu          = 0x10000000,  // ditto
+};
+
+template <typename Asm, typename Rtype, typename Vtype>
+void TemplateLoadConst32(Asm* a, Rtype rd, Vtype value) {
+  if (IsUint<16>(value)) {
+    // Use OR with (unsigned) immediate to encode 16b unsigned int.
+#ifdef  MIPS_LOAD_CONST_PATH
+    a->loadConst32Path |= kMipsPathOri;
+#endif
+    a->Ori(rd, ZERO, value);
+  } else if (IsInt<16>(value)) {
+    // Use ADD with (signed) immediate to encode 16b signed int.
+#ifdef  MIPS_LOAD_CONST_PATH
+    a->loadConst32Path |= kMipsPathAddiu;
+#endif
+    a->Addiu(rd, ZERO, value);
+  } else {
+    a->Lui(rd, value >> 16);
+    if (value & 0xFFFF) {
+#ifdef  MIPS_LOAD_CONST_PATH
+      a->loadConst32Path |= kMipsPathLuiOri;
+#endif
+      a->Ori(rd, rd, value);
+#ifdef  MIPS_LOAD_CONST_PATH
+    } else {
+      a->loadConst32Path |= kMipsPathLui;
+#endif
+    }
+  }
+}
+
+template <typename Vtype>
+int InstrCountForLoadReplicatedConst32(Vtype value) {
+  int32_t x = Low32Bits(value);
+  int32_t y = High32Bits(value);
+
+  if (x == y) {
+    return (IsUint<16>(x) || IsInt<16>(x) || ((x & 0xFFFF) == 0 && IsInt<16>(value >> 16))) ? 2 : 3;
+  }
+
+  return INT_MAX;
+}
+
+template <typename Asm, typename Rtype>
+void LoadReplicatedConst32(Asm *a, Rtype rd, int64_t value) {
+  int32_t x = Low32Bits(value);
+  int32_t y = High32Bits(value);
+  CHECK_EQ(x, y);
+
+  if (IsUint<16>(x)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathOriDinsu;
+#endif
+    a->Ori(rd, ZERO, x);
+  } else if (IsInt<16>(x)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathDaddiuDinsu;
+#endif
+    a->Daddiu(rd, ZERO, x);
+  } else {
+#ifdef  MIPS_LOAD_CONST_PATH
+    a->loadConst32Path = kMipsPathZero;
+#endif
+    a->LoadConst32(rd, x);
+#ifdef  MIPS_LOAD_CONST_PATH
+    DCHECK(a->loadConst32Path == kMipsPathLui ||
+           a->loadConst32Path == kMipsPathLuiOri) << a->loadConst32Path;
+    if (a->loadConst32Path == kMipsPathLui) {
+      a->loadConst64Path |= kMipsPathLuiDinsu;
+    } else {
+      a->loadConst64Path |= kMipsPathLuiOriDinsu;
+    }
+#endif
+  }
+  a->Dinsu(rd, rd, 32, 32);
+}
+
+template <typename Asm, typename Rtype, typename Vtype>
+void TemplateLoadConst64(Asm* a, Rtype rd, Vtype value) {
+  int bit31 = (value & UINT64_C(0x80000000)) != 0;
+  int rep32_count = InstrCountForLoadReplicatedConst32(value);
+  uint64_t tmp2 = value;
+  uint64_t tmp3;
+  int32_t tmp2_int32 = value;
+
+  if (bit31) {
+    tmp2 += UINT64_C(0x100000000);
+  }
+  if (tmp2 & UINT64_C(0x800000000000)) {
+    tmp3 = tmp2 + UINT64_C(0x1000000000000);
+  } else {
+    tmp3 = tmp2;
+  }
+  tmp3 >>= 48;
+
+  // Loads with 1 instruction.
+  if (IsUint<16>(value)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathOri;
+#endif
+    a->Ori(rd, ZERO, value);
+  } else if (IsInt<16>(value)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathDaddiu;
+#endif
+    a->Daddiu(rd, ZERO, value);
+  } else if ((value & 0xFFFF) == 0 && IsInt<16>(value >> 16)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathLui;
+#endif
+    a->Lui(rd, value >> 16);
+  } else if (IsInt<32>(value)) {
+    // Loads with 2 instructions.
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathLuiOri;
+#endif
+    a->Lui(rd, value >> 16);
+    a->Ori(rd, rd, value);
+  } else if ((value & 0xFFFF0000) == 0 && IsInt<16>(value >> 32)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathOriDahi;
+#endif
+    a->Ori(rd, ZERO, value);
+    a->Dahi(rd, value >> 32);
+  } else if ((value & UINT64_C(0xFFFFFFFF0000)) == 0) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathOriDati;
+#endif
+    a->Ori(rd, ZERO, value);
+    a->Dati(rd, value >> 48);
+  } else if ((value & 0xFFFF) == 0 &&
+             (-32768 - bit31) <= (value >> 32) && (value >> 32) <= (32767 - bit31)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathLuiDahi;
+#endif
+    a->Lui(rd, value >> 16);
+    a->Dahi(rd, (value >> 32) + bit31);
+  } else if ((value & 0xFFFF) == 0 && ((value >> 31) & 0x1FFFF) == ((0x20000 - bit31) & 0x1FFFF)) {
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathLuiDati;
+#endif
+    a->Lui(rd, value >> 16);
+    a->Dati(rd, (value >> 48) + bit31);
+  } else if (IsPowerOfTwo(value + UINT64_C(1))) {
+    int shift_cnt = 64 - CTZ(value + UINT64_C(1));
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathDaddiuDsrlX;
+#endif
+    a->Daddiu(rd, ZERO, -1);
+    if (shift_cnt < 32) {
+      a->Dsrl(rd, rd, shift_cnt);
+    } else {
+      a->Dsrl32(rd, rd, shift_cnt & 31);
+    }
+  } else {
+    int shift_cnt = CTZ(value);
+    int64_t tmp = value >> shift_cnt;
+#ifdef MIPS_LOAD_CONST_PATH
+    a->loadConst64Path |= kMipsPathOriDsllX;
+#endif
+    if (IsUint<16>(tmp)) {
+      a->Ori(rd, ZERO, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else if (IsInt<16>(tmp)) {
+#ifdef MIPS_LOAD_CONST_PATH
+      a->loadConst64Path |= kMipsPathDaddiuDsllX;
+#endif
+      a->Daddiu(rd, ZERO, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else if (rep32_count < 3) {
+      LoadReplicatedConst32(a, rd, value);
+    } else if (IsInt<16>(tmp2_int32) && (((tmp2 >> 32) & 0xFFFF) != 0) && (tmp3 == 0)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+      a->loadConst64Path |= kMipsPathDaddiuDahi;
+#endif
+      a->Daddiu(rd, ZERO, tmp2 & 0xFFFF);
+      a->Dahi(rd, tmp2 >> 32);
+    } else if (IsInt<16>(tmp2_int32) && (((tmp2 >> 32) & 0xFFFF) == 0) && (tmp3 != 0)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+      a->loadConst64Path |= kMipsPathDaddiuDati;
+#endif
+      a->Daddiu(rd, ZERO, tmp2 & 0xFFFF);
+      a->Dati(rd, tmp3);
+    } else if (IsInt<32>(tmp)) {
+#ifdef MIPS_LOAD_CONST_PATH
+      a->loadConst64Path |= kMipsPathLuiOriDsllX;
+#endif
+      // Loads with 3 instructions.
+      a->Lui(rd, tmp >> 16);
+      a->Ori(rd, rd, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else {
+      shift_cnt = 16 + CTZ(value >> 16);
+      tmp = value >> shift_cnt;
+      if (IsUint<16>(tmp)) {
+#ifdef MIPS_LOAD_CONST_PATH
+        a->loadConst64Path |= kMipsPathOriDsllXOri;
+#endif
+        a->Ori(rd, ZERO, tmp);
+        if (shift_cnt < 32) {
+          a->Dsll(rd, rd, shift_cnt);
+        } else {
+          a->Dsll32(rd, rd, shift_cnt & 31);
+        }
+        a->Ori(rd, rd, value);
+      } else if (IsInt<16>(tmp)) {
+#ifdef MIPS_LOAD_CONST_PATH
+        a->loadConst64Path |= kMipsPathDaddiuDsllXOri;
+#endif
+        a->Daddiu(rd, ZERO, tmp);
+        if (shift_cnt < 32) {
+          a->Dsll(rd, rd, shift_cnt);
+        } else {
+          a->Dsll32(rd, rd, shift_cnt & 31);
+        }
+        a->Ori(rd, rd, value);
+      } else if (rep32_count < 4) {
+        LoadReplicatedConst32(a, rd, value);
+      } else {
+        // Loads with 3-4 instructions.
+#ifdef  MIPS_LOAD_CONST_PATH
+        enum LoadConst64Path path1;
+        enum LoadConst64Path path2 = kMipsPathZero;
+        enum LoadConst64Path path3 = kMipsPathZero;
+#endif
+        if (IsUint<16>(tmp2_int32)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+          path1 = kMipsPathOri;
+#endif
+          a->Ori(rd, ZERO, tmp2 & 0xFFFF);
+        } else if (IsInt<16>(tmp2_int32)) {
+#ifdef  MIPS_LOAD_CONST_PATH
+          path1 = kMipsPathDaddiu;
+#endif
+          a->Daddiu(rd, ZERO, tmp2 & 0xFFFF);
+        } else {
+          a->Lui(rd, tmp2 >> 16);
+          if ((tmp2 & 0xFFFF) != 0) {
+#ifdef  MIPS_LOAD_CONST_PATH
+            path1 = kMipsPathLuiOri;
+#endif
+            a->Ori(rd, rd, tmp2 & 0xFFFF);
+#ifdef  MIPS_LOAD_CONST_PATH
+          } else {
+            path1 = kMipsPathLui;
+#endif
+          }
+        }
+        if (((tmp2 >> 32) & 0xFFFF) != 0) {
+#ifdef  MIPS_LOAD_CONST_PATH
+          path2 = kMipsPathDahi;
+#endif
+          a->Dahi(rd, tmp2 >> 32);
+        }
+        if (tmp3 != 0) {
+#ifdef  MIPS_LOAD_CONST_PATH
+          path3 = kMipsPathDati;
+#endif
+          a->Dati(rd, tmp3);
+        }
+#ifdef  MIPS_LOAD_CONST_PATH
+        if (path1 == kMipsPathOri) {
+          if (path2 == kMipsPathDahi) {
+            CHECK_EQ(path3, kMipsPathDati) << std::hex << "0x" << (path1|path2|path3);
+            a->loadConst64Path |= kMipsPathOriDahiDati;
+          } else {
+            CHECK_EQ(path2, kMipsPathDahi) << std::hex << "0x" << (path1|path2|path3);
+          }
+        } else if (path1 == kMipsPathDaddiu) {
+          if (path2 == kMipsPathDahi) {
+            CHECK_EQ(path3, kMipsPathDati) << std::hex << "0x" << (path1|path2|path3);
+            a->loadConst64Path |= kMipsPathDaddiuDahiDati;
+          } else {
+            CHECK_EQ(path2, kMipsPathDahi) << std::hex << "0x" << (path1|path2|path3);
+          }
+        } else if (path1 == kMipsPathLuiOri) {
+          if (path2 == kMipsPathDahi) {
+            if (path3 == kMipsPathDati) {
+              a->loadConst64Path |= kMipsPathLuiOriDahiDati;
+            } else {
+              a->loadConst64Path |= kMipsPathLuiOriDahi;
+            }
+          } else {
+            CHECK_EQ(path3, kMipsPathDati) << std::hex << "0x" << (path1|path2|path3);
+            a->loadConst64Path |= kMipsPathLuiOriDati;
+          }
+        } else if (path1 == kMipsPathLui) {
+          if (path2 == kMipsPathDahi) {
+            CHECK_EQ(path3, kMipsPathDati) << std::hex << "0x" << (path1|path2|path3);
+            a->loadConst64Path |= kMipsPathLuiDahiDati;
+          } else {
+            CHECK_EQ(path2, kMipsPathDahi) << std::hex << "0x" << (path1|path2|path3);
+          }
+        }
+#endif  /* MIPS_LOAD_CONST_PATH */
+      }
+    }
+  }
+}
+
 static constexpr size_t kMips64WordSize = 4;
 static constexpr size_t kMips64DoublewordSize = 8;
 
@@ -321,6 +665,7 @@ class Mips64Assembler FINAL : public Assembler {
   void Not(GpuRegister rd, GpuRegister rs);
 
   // Higher level composite instructions.
+  int InstrCountForLoadReplicatedConst32(int64_t);
   void LoadConst32(GpuRegister rd, int32_t value);
   void LoadConst64(GpuRegister rd, int64_t value);  // MIPS64
 

anestisb/oatdump_plus

  o Make the implementation of LoadConst64() into a template.
Test: Booted MIPS64 in QEMU, and ran assembler tests.

compiler/utils/mips64/assembler_mips64.h
@@ -33,6 +33,237 @@
 namespace art {
 namespace mips64 {
 
+enum LoadConst64Path {
+  kLoadConst64PathZero           = 0x0,
+  kLoadConst64PathOri            = 0x1,
+  kLoadConst64PathDaddiu         = 0x2,
+  kLoadConst64PathLui            = 0x4,
+  kLoadConst64PathLuiOri         = 0x8,
+  kLoadConst64PathOriDahi        = 0x10,
+  kLoadConst64PathOriDati        = 0x20,
+  kLoadConst64PathLuiDahi        = 0x40,
+  kLoadConst64PathLuiDati        = 0x80,
+  kLoadConst64PathDaddiuDsrlX    = 0x100,
+  kLoadConst64PathOriDsllX       = 0x200,
+  kLoadConst64PathDaddiuDsllX    = 0x400,
+  kLoadConst64PathLuiOriDsllX    = 0x800,
+  kLoadConst64PathOriDsllXOri    = 0x1000,
+  kLoadConst64PathDaddiuDsllXOri = 0x2000,
+  kLoadConst64PathDaddiuDahi     = 0x4000,
+  kLoadConst64PathDaddiuDati     = 0x8000,
+  kLoadConst64PathDinsu1         = 0x10000,
+  kLoadConst64PathDinsu2         = 0x20000,
+  kLoadConst64PathCatchAll       = 0x40000,
+  kLoadConst64PathAllPaths       = 0x7ffff,
+};
+
+template <typename Asm>
+void TemplateLoadConst32(Asm* a, GpuRegister rd, int32_t value) {
+  if (IsUint<16>(value)) {
+    // Use OR with (unsigned) immediate to encode 16b unsigned int.
+    a->Ori(rd, ZERO, value);
+  } else if (IsInt<16>(value)) {
+    // Use ADD with (signed) immediate to encode 16b signed int.
+    a->Addiu(rd, ZERO, value);
+  } else {
+    // Set 16 most significant bits of value. The "lui" instruction
+    // also clears the 16 least significant bits to zero.
+    a->Lui(rd, value >> 16);
+    if (value & 0xFFFF) {
+      // If the 16 least significant bits are non-zero, set them
+      // here.
+      a->Ori(rd, rd, value);
+    }
+  }
+}
+
+static inline int InstrCountForLoadReplicatedConst32(int64_t value) {
+  int32_t x = Low32Bits(value);
+  int32_t y = High32Bits(value);
+
+  if (x == y) {
+    return (IsUint<16>(x) || IsInt<16>(x) || ((x & 0xFFFF) == 0 && IsInt<16>(value >> 16))) ? 2 : 3;
+  }
+
+  return INT_MAX;
+}
+
+template <typename Asm, typename Rtype, typename Vtype>
+void TemplateLoadConst64(Asm* a, Rtype rd, Vtype value) {
+  int bit31 = (value & UINT64_C(0x80000000)) != 0;
+  int rep32_count = InstrCountForLoadReplicatedConst32(value);
+
+  // Loads with 1 instruction.
+  if (IsUint<16>(value)) {
+    // 64-bit value can be loaded as an unsigned 16-bit number.
+    a->RecordLoadConst64Path(kLoadConst64PathOri);
+    a->Ori(rd, ZERO, value);
+  } else if (IsInt<16>(value)) {
+    // 64-bit value can be loaded as an signed 16-bit number.
+    a->RecordLoadConst64Path(kLoadConst64PathDaddiu);
+    a->Daddiu(rd, ZERO, value);
+  } else if ((value & 0xFFFF) == 0 && IsInt<16>(value >> 16)) {
+    // 64-bit value can be loaded as an signed 32-bit number which has all
+    // of its 16 least significant bits set to zero.
+    a->RecordLoadConst64Path(kLoadConst64PathLui);
+    a->Lui(rd, value >> 16);
+  } else if (IsInt<32>(value)) {
+    // Loads with 2 instructions.
+    // 64-bit value can be loaded as an signed 32-bit number which has some
+    // or all of its 16 least significant bits set to one.
+    a->RecordLoadConst64Path(kLoadConst64PathLuiOri);
+    a->Lui(rd, value >> 16);
+    a->Ori(rd, rd, value);
+  } else if ((value & 0xFFFF0000) == 0 && IsInt<16>(value >> 32)) {
+    // 64-bit value which consists of an unsigned 16-bit value in its
+    // least significant 32-bits, and a signed 16-bit value in its
+    // most significant 32-bits.
+    a->RecordLoadConst64Path(kLoadConst64PathOriDahi);
+    a->Ori(rd, ZERO, value);
+    a->Dahi(rd, value >> 32);
+  } else if ((value & UINT64_C(0xFFFFFFFF0000)) == 0) {
+    // 64-bit value which consists of an unsigned 16-bit value in its
+    // least significant 48-bits, and a signed 16-bit value in its
+    // most significant 16-bits.
+    a->RecordLoadConst64Path(kLoadConst64PathOriDati);
+    a->Ori(rd, ZERO, value);
+    a->Dati(rd, value >> 48);
+  } else if ((value & 0xFFFF) == 0 &&
+             (-32768 - bit31) <= (value >> 32) && (value >> 32) <= (32767 - bit31)) {
+    // 16 LSBs (Least Significant Bits) all set to zero.
+    // 48 MSBs (Most Significant Bits) hold a signed 32-bit value.
+    a->RecordLoadConst64Path(kLoadConst64PathLuiDahi);
+    a->Lui(rd, value >> 16);
+    a->Dahi(rd, (value >> 32) + bit31);
+  } else if ((value & 0xFFFF) == 0 && ((value >> 31) & 0x1FFFF) == ((0x20000 - bit31) & 0x1FFFF)) {
+    // 16 LSBs all set to zero.
+    // 48 MSBs hold a signed value which can't be represented by signed
+    // 32-bit number, and the middle 16 bits are all zero, or all one.
+    a->RecordLoadConst64Path(kLoadConst64PathLuiDati);
+    a->Lui(rd, value >> 16);
+    a->Dati(rd, (value >> 48) + bit31);
+  } else if (IsInt<16>(static_cast<int32_t>(value)) &&
+             (-32768 - bit31) <= (value >> 32) && (value >> 32) <= (32767 - bit31)) {
+    // 32 LSBs contain an unsigned 16-bit number.
+    // 32 MSBs contain a signed 16-bit number.
+    a->RecordLoadConst64Path(kLoadConst64PathDaddiuDahi);
+    a->Daddiu(rd, ZERO, value);
+    a->Dahi(rd, (value >> 32) + bit31);
+  } else if (IsInt<16>(static_cast<int32_t>(value)) &&
+             ((value >> 31) & 0x1FFFF) == ((0x20000 - bit31) & 0x1FFFF)) {
+    // 48 LSBs contain an unsigned 16-bit number.
+    // 16 MSBs contain a signed 16-bit number.
+    a->RecordLoadConst64Path(kLoadConst64PathDaddiuDati);
+    a->Daddiu(rd, ZERO, value);
+    a->Dati(rd, (value >> 48) + bit31);
+  } else if (IsPowerOfTwo(value + UINT64_C(1))) {
+    // 64-bit values which have their "n" MSBs set to one, and their
+    // "64-n" LSBs set to zero. "n" must meet the restrictions 0 < n < 64.
+    int shift_cnt = 64 - CTZ(value + UINT64_C(1));
+    a->RecordLoadConst64Path(kLoadConst64PathDaddiuDsrlX);
+    a->Daddiu(rd, ZERO, -1);
+    if (shift_cnt < 32) {
+      a->Dsrl(rd, rd, shift_cnt);
+    } else {
+      a->Dsrl32(rd, rd, shift_cnt & 31);
+    }
+  } else {
+    int shift_cnt = CTZ(value);
+    int64_t tmp = value >> shift_cnt;
+    a->RecordLoadConst64Path(kLoadConst64PathOriDsllX);
+    if (IsUint<16>(tmp)) {
+      // Value can be computed by loading a 16-bit unsigned value, and
+      // then shifting left.
+      a->Ori(rd, ZERO, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else if (IsInt<16>(tmp)) {
+      // Value can be computed by loading a 16-bit signed value, and
+      // then shifting left.
+      a->RecordLoadConst64Path(kLoadConst64PathDaddiuDsllX);
+      a->Daddiu(rd, ZERO, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else if (rep32_count < 3) {
+      // Value being loaded has 32 LSBs equal to the 32 MSBs, and the
+      // value loaded into the 32 LSBs can be loaded with a single
+      // MIPS instruction.
+      a->LoadConst32(rd, value);
+      a->Dinsu(rd, rd, 32, 32);
+      a->RecordLoadConst64Path(kLoadConst64PathDinsu1);
+    } else if (IsInt<32>(tmp)) {
+      // Loads with 3 instructions.
+      // Value can be computed by loading a 32-bit signed value, and
+      // then shifting left.
+      a->RecordLoadConst64Path(kLoadConst64PathLuiOriDsllX);
+      a->Lui(rd, tmp >> 16);
+      a->Ori(rd, rd, tmp);
+      if (shift_cnt < 32) {
+        a->Dsll(rd, rd, shift_cnt);
+      } else {
+        a->Dsll32(rd, rd, shift_cnt & 31);
+      }
+    } else {
+      shift_cnt = 16 + CTZ(value >> 16);
+      tmp = value >> shift_cnt;
+      if (IsUint<16>(tmp)) {
+        // Value can be computed by loading a 16-bit unsigned value,
+        // shifting left, and "or"ing in another 16-bit unsigned value.
+        a->RecordLoadConst64Path(kLoadConst64PathOriDsllXOri);
+        a->Ori(rd, ZERO, tmp);
+        if (shift_cnt < 32) {
+          a->Dsll(rd, rd, shift_cnt);
+        } else {
+          a->Dsll32(rd, rd, shift_cnt & 31);
+        }
+        a->Ori(rd, rd, value);
+      } else if (IsInt<16>(tmp)) {
+        // Value can be computed by loading a 16-bit signed value,
+        // shifting left, and "or"ing in a 16-bit unsigned value.
+        a->RecordLoadConst64Path(kLoadConst64PathDaddiuDsllXOri);
+        a->Daddiu(rd, ZERO, tmp);
+        if (shift_cnt < 32) {
+          a->Dsll(rd, rd, shift_cnt);
+        } else {
+          a->Dsll32(rd, rd, shift_cnt & 31);
+        }
+        a->Ori(rd, rd, value);
+      } else if (rep32_count < 4) {
+        // Value being loaded has 32 LSBs equal to the 32 MSBs, and the
+        // value in the 32 LSBs requires 2 MIPS instructions to load.
+        a->LoadConst32(rd, value);
+        a->Dinsu(rd, rd, 32, 32);
+        a->RecordLoadConst64Path(kLoadConst64PathDinsu2);
+      } else {
+        // Loads with 3-4 instructions.
+        // Catch-all case to get any other 64-bit values which aren't
+        // handled by special cases above.
+        uint64_t tmp2 = value;
+        a->RecordLoadConst64Path(kLoadConst64PathCatchAll);
+        a->LoadConst32(rd, value);
+        if (bit31) {
+          tmp2 += UINT64_C(0x100000000);
+        }
+        if (((tmp2 >> 32) & 0xFFFF) != 0) {
+          a->Dahi(rd, tmp2 >> 32);
+        }
+        if (tmp2 & UINT64_C(0x800000000000)) {
+          tmp2 += UINT64_C(0x1000000000000);
+        }
+        if ((tmp2 >> 48) != 0) {
+          a->Dati(rd, tmp2 >> 48);
+        }
+      }
+    }
+  }
+}
+
 static constexpr size_t kMips64WordSize = 4;
 static constexpr size_t kMips64DoublewordSize = 8;
 
@@ -326,8 +557,12 @@ class Mips64Assembler FINAL : public Assembler, public JNIMacroAssembler<Pointer
   void Not(GpuRegister rd, GpuRegister rs);
 
   // Higher level composite instructions.
+  int InstrCountForLoadReplicatedConst32(int64_t);
   void LoadConst32(GpuRegister rd, int32_t value);
   void LoadConst64(GpuRegister rd, int64_t value);  // MIPS64
+
+  // This function is only used for testing purposes.
+  void RecordLoadConst64Path(int value);
 
   void Daddiu64(GpuRegister rt, GpuRegister rs, int64_t value, GpuRegister rtmp = AT);  // MIPS64
 