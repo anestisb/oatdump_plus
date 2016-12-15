b81a9840b44480bfeacd74b8d9f51e06f295411d
Alex Light <allight@google.com>
2016-12-15 00:59:05 +0000

    Revert "Basic obsolete methods support"
    This reverts commit d8936da27b792d1ca02e59c92456a1a53c7b9905.
    
    Reason for revert: Some sort of race in JIT
    
    Change-Id: Ibb4e520bb0721d6d7aa2c841a52eb5baff07740e

--- test/Android.run-test.mk
+++ test/Android.run-test.mk
@@ -280,9 +280,6 @@ TEST_ART_BROKEN_TARGET_TESTS += \
   911-get-stack-trace \
   912-classes \
   913-heaps \
-  914-hello-obsolescence \
-  915-obsolete-2 \
-  916-obsolete-jit \
 
 ifneq (,$(filter target,$(TARGET_TYPES)))
   ART_TEST_KNOWN_BROKEN += $(call all-run-test-names,target,$(RUN_TYPES),$(PREBUILD_TYPES), \
