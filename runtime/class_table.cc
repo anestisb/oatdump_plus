eca0dea9f103a19042f0d3505af1126ac97be0dd
Stephen Hines <srhines@google.com>
2017-02-08 11:19:23 -0800

    Revert -Wunreachable-code workaround.
    Bug: http://b/31104323
    Test: Built/ran ART successfully on sailfish.
    
    This reverts commit 813b9c47d8952b3f02a6032d79d66433c981d030, which
    added a workaround for Clang mistriggering -Wunreachable-code.
    
    Change-Id: I46636c9b4f574a870c1f95b6160d05f2a75f8ab8

--- runtime/class_table.cc
+++ runtime/class_table.cc
@@ -55,10 +55,6 @@ mirror::Class* ClassTable::LookupByDescriptor(ObjPtr<mirror::Class> klass) {
   return nullptr;
 }
 
-// Bug: http://b/31104323 Ignore -Wunreachable-code from the for loop below
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunreachable-code"
-
 mirror::Class* ClassTable::UpdateClass(const char* descriptor, mirror::Class* klass, size_t hash) {
   WriterMutexLock mu(Thread::Current(), lock_);
   // Should only be updating latest table.
@@ -83,8 +79,6 @@ mirror::Class* ClassTable::UpdateClass(const char* descriptor, mirror::Class* kl
   *existing_it = TableSlot(klass, hash);
   return existing;
 }
-
-#pragma clang diagnostic pop  // http://b/31104323
 
 size_t ClassTable::CountDefiningLoaderClasses(ObjPtr<mirror::ClassLoader> defining_loader,
                                               const ClassSet& set) const {
