0b66d6174bf1f6023f9d36dda8538490b79c2e9f
Vladimir Marko <vmarko@google.com>
2017-03-13 14:50:04 +0000

    Revert^5 "Hash-based dex cache type array."
    For app images, ImageWriter does not add boot image
    classes to the app image class table even though it
    keeps them in the dex caches. The reason for that is
    unknown, the code looks OK.
    
    Bug: 34839984
    Bug: 30627598
    Bug: 34659969
    
    Also reverts "Improve debugging output for a crash."
    
    This reverts commits
        bfb80d25eaeb7a604d5dd25a370e3869e96a33ab,
        8dd56fcb3196f466ecaffd445397cb11ef85f89f.
    
    Test: testrunner.py --host
    Change-Id: Ic8db128207c07588c7f11563208ae1e85c8b0e84

--- runtime/image.cc
+++ runtime/image.cc
@@ -25,7 +25,7 @@
 namespace art {
 
 const uint8_t ImageHeader::kImageMagic[] = { 'a', 'r', 't', '\n' };
-const uint8_t ImageHeader::kImageVersion[] = { '0', '4', '0', '\0' };  // Integer.valueOf intrinsic
+const uint8_t ImageHeader::kImageVersion[] = { '0', '4', '1', '\0' };  // Revert DexCache types.
 
 ImageHeader::ImageHeader(uint32_t image_begin,
                          uint32_t image_size,
