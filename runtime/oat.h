222b3f1bda77e5d0e65f180f627cefa2b4100639
Nicolas Geoffray <ngeoffray@google.com>
2017-05-05 10:27:58 +0100

    Bump oat version after compiler filter name changes.
    bug:37988580
    Test: build
    
    (cherry picked from commit f977691961b5a49a074a535fcb29a5ad4a318974)
    
    Change-Id: If52e569b6403a8b3f2e31d472b6d11ab42d13763

--- runtime/oat.h
+++ runtime/oat.h
@@ -32,7 +32,7 @@ class InstructionSetFeatures;
 class PACKED(4) OatHeader {
  public:
   static constexpr uint8_t kOatMagic[] = { 'o', 'a', 't', '\n' };
-  static constexpr uint8_t kOatVersion[] = { '1', '1', '9', '\0' };  // Add thread_local_limit.
+  static constexpr uint8_t kOatVersion[] = { '1', '2', '4', '\0' };  // New compiler filter names.
 
   static constexpr const char* kImageLocationKey = "image-location";
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
