03ba5d67df7568fd8c60d171f1a20c443bae975e
Igor Murashkin <iam@google.com>
2017-06-27 16:00:55 -0700

    test: Modify 633-checker-rtp-getclass for javac/dx
    Previously 1 of the methods were failing checker with javac/dx.
    Move their old bytecode to a smali file to retain testing of those
    optimizations.
    
    Rewrite the checker tests in Main.java to use the javac/dx-generated
    bytecode.
    
    Test: art/test/run-test --64 --host --optimizing --build-with-javac-dx 633-checker-rtp-getclass
    Bug: 62950048
    Bug: 36902714
    Change-Id: I26c6475609b6f6d76e7339d9824cb5daef9828ca

--- test/633-checker-rtp-getclass/src/Main.java
+++ test/633-checker-rtp-getclass/src/Main.java
@@ -14,34 +14,13 @@
  * limitations under the License.
  */
 
+import java.lang.reflect.Method;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println($opt$noinline$foo(new Main()));
-    System.out.println($opt$noinline$foo(new SubMain()));
-    System.out.println($opt$noinline$foo(new SubSubMain()));
-  }
-
-
-  // Checker test to make sure the only inlined instruction is
-  // SubMain.bar.
-  /// CHECK-START: int Main.$opt$noinline$foo(Main) inliner (after)
-  /// CHECK-DAG:                InvokeVirtual method_name:Main.foo
-  /// CHECK-DAG: <<Const:i\d+>> IntConstant 3
-  /// CHECK:                    begin_block
-  /// CHECK:                    BoundType klass:SubMain
-  /// CHECK:                    Return [<<Const>>]
-  /// CHECK-NOT:                begin_block
-  /// CHECK:                    end_block
-  public static int $opt$noinline$foo(Main o) {
-    if (doThrow) { throw new Error(); }
-    // To exercise the bug on Jack, we need two getClass compares.
-    if (o.getClass() == Main.class || o.getClass() != SubMain.class) {
-      return o.foo();
-    } else {
-      // We used to wrongly bound the type of o to `Main` here and then realize that's
-      // impossible and mark this branch as dead.
-      return o.bar();
-    }
+    System.out.println($noinline$runSmaliTest("$opt$noinline$foo", new Main()));
+    System.out.println($noinline$runSmaliTest("$opt$noinline$foo", new SubMain()));
+    System.out.println($noinline$runSmaliTest("$opt$noinline$foo", new SubSubMain()));
   }
 
   public int bar() {
@@ -53,6 +32,16 @@ public class Main {
   }
 
   public static boolean doThrow = false;
+
+  public static int $noinline$runSmaliTest(String name, Main input) {
+    try {
+      Class<?> c = Class.forName("SmaliTests");
+      Method m = c.getMethod(name, Main.class);
+      return (Integer) m.invoke(null, input);
+    } catch (Exception ex) {
+      throw new Error(ex);
+    }
+  }
 }
 
 class SubMain extends Main {
