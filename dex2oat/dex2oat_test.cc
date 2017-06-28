1fb4a7b5d1c2a315b8cffcc56c9b66c77b71b24b
Nicolas Geoffray <ngeoffray@google.com>
2017-06-28 10:59:25 +0100

    [DO NOT MERGE] Disable non-functional test in oc-dev.
    bug: 37318304
    bug: 63064652
    
    Test: dex2oat_test
    Change-Id: I4f5443a3beb9405ffd2172b0df2f7d948b9cd0f8

--- dex2oat/dex2oat_test.cc
+++ dex2oat/dex2oat_test.cc
@@ -883,7 +883,8 @@ class Dex2oatReturnCodeTest : public Dex2oatTest {
   }
 };
 
-TEST_F(Dex2oatReturnCodeTest, TestCreateRuntime) {
+// Disabled test due to b/37318304.
+TEST_F(Dex2oatReturnCodeTest, DISABLED_TestCreateRuntime) {
   int status = RunTest({ "--boot-image=/this/does/not/exist/yolo.oat" });
   EXPECT_EQ(static_cast<int>(dex2oat::ReturnCode::kCreateRuntime), WEXITSTATUS(status)) << output_;
 }
