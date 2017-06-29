hopez13/art

If branch changer which changes the opcodes.
This program mutates the program by changing the current if comparision
operators.

Test: not performed
Change-Id: I1b8a3009bc829678d84f3f2fb42d0e6ba3deeb0a

tools/dexfuzz/src/dexfuzz/program/Program.java
@@ -27,6 +27,7 @@ import dexfuzz.program.mutators.CodeMutator;
 import dexfuzz.program.mutators.ConstantValueChanger;
 import dexfuzz.program.mutators.ConversionRepeater;
 import dexfuzz.program.mutators.FieldFlagChanger;
+import dexfuzz.program.mutators.IfBranchChanger;
 import dexfuzz.program.mutators.InstructionDeleter;
 import dexfuzz.program.mutators.InstructionDuplicator;
 import dexfuzz.program.mutators.InstructionSwapper;
@@ -194,6 +195,7 @@ public class Program {
     registerMutator(new ConstantValueChanger(rng, mutationStats, mutations));
     registerMutator(new ConversionRepeater(rng, mutationStats, mutations));
     registerMutator(new FieldFlagChanger(rng, mutationStats, mutations));
+    registerMutator(new IfBranchChanger(rng, mutationStats, mutations));
     registerMutator(new InstructionDeleter(rng, mutationStats, mutations));
     registerMutator(new InstructionDuplicator(rng, mutationStats, mutations));
     registerMutator(new InstructionSwapper(rng, mutationStats, mutations));

anestisb/oatdump_plus

Test: run dexfuzz with the two mutations

tools/dexfuzz/src/dexfuzz/program/Program.java
@@ -32,7 +32,9 @@ import dexfuzz.program.mutators.InstructionDuplicator;
 import dexfuzz.program.mutators.InstructionSwapper;
 import dexfuzz.program.mutators.NewMethodCaller;
 import dexfuzz.program.mutators.NonsenseStringPrinter;
+import dexfuzz.program.mutators.OppositeBranchChanger;
 import dexfuzz.program.mutators.PoolIndexChanger;
+import dexfuzz.program.mutators.RandomBranchChanger;
 import dexfuzz.program.mutators.RandomInstructionGenerator;
 import dexfuzz.program.mutators.SwitchBranchShifter;
 import dexfuzz.program.mutators.TryBlockShifter;
@@ -199,7 +201,9 @@ public class Program {
     registerMutator(new InstructionSwapper(rng, mutationStats, mutations));
     registerMutator(new NewMethodCaller(rng, mutationStats, mutations));
     registerMutator(new NonsenseStringPrinter(rng, mutationStats, mutations));
+    registerMutator(new OppositeBranchChanger(rng, mutationStats, mutations));
     registerMutator(new PoolIndexChanger(rng, mutationStats, mutations));
+    registerMutator(new RandomBranchChanger(rng, mutationStats, mutations));
     registerMutator(new RandomInstructionGenerator(rng, mutationStats, mutations));
     registerMutator(new SwitchBranchShifter(rng, mutationStats, mutations));
     registerMutator(new TryBlockShifter(rng, mutationStats, mutations));