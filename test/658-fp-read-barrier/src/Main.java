bc4b013b2fceec81b094775400ce210af23188d6
Nicolas Geoffray <ngeoffray@google.com>
2017-07-03 13:26:57 +0100

    Address missed review comments.
    Test: test.py
    Change-Id: I055c4fca40b9ef28ff8fc74c3e2b1001928fffc9

--- test/658-fp-read-barrier/src/Main.java
+++ test/658-fp-read-barrier/src/Main.java
@@ -37,7 +37,7 @@ public class Main {
       Object o;
     };
     // Make the thread a daemon to quit early in case of an
-    // exception throw below.
+    // exception thrown below.
     t.setDaemon(true);
     t.start();
 
@@ -89,7 +89,9 @@ public class Main {
     // the read barrier stub used to not restore the D registers.
     double p = main.objectField.field1;
 
-    return p + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 + d13 + d14 + d15 + d16 + d17 + d18 + d19 + d20 + d21 + d22 + d23 + d24 + d25 + d26 + d27 + d28 + d29 + d30 + d31 + d32;
+    return p + d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12 +
+        d13 + d14 + d15 + d16 + d17 + d18 + d19 + d20 + d21 + d22 + d23 + d24 +
+        d25 + d26 + d27 + d28 + d29 + d30 + d31 + d32;
   }
 
   // Initialize objects here and not in 'main' to avoid having
