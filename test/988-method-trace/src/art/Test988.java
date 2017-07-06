hopez13/art

test: Add tests for tracing compiler intrinsics
Ensure that ART TI MethodEntry/MethodExit events are correctly
reported for all intrinsics (for public api).

Test: art/test/run-test --host --optimizing 988-method-trace
Change-Id: Iab90e9cfffb3c644df7fffac26f35a66b95e299a

test/988-method-trace/src/art/Test988.java
@@ -55,7 +55,7 @@ public class Test988 {
         }
         @Override
         public void Print() {
-            System.out.println(whitespace(cnt) + "=> " + m);
+            System.out.println(whitespace(cnt) + "=> " + methodToString(m));
         }
     }
 
@@ -113,6 +113,13 @@ public class Test988 {
       }
     }
 
+    static String methodToString(Object m) {
+      // Make the output more similar between ART and RI,
+      // by removing the 'native' specifier from methods.
+      String methodStr = m.toString();
+      return methodStr.replaceFirst(" native", "");
+    }
+
     static final class MethodReturn implements Printable {
         private Object m;
         private Object val;
@@ -135,7 +142,7 @@ public class Test988 {
               klass = val.getClass();
             }
             System.out.println(
-                whitespace(cnt) + "<= " + m + " -> <" + klass + ": " + print + ">");
+                whitespace(cnt) + "<= " + methodToString(m) + " -> <" + klass + ": " + print + ">");
         }
     }
 
@@ -148,7 +155,7 @@ public class Test988 {
         }
         @Override
         public void Print() {
-            System.out.println(whitespace(cnt) + "<= " + m + " EXCEPTION");
+            System.out.println(whitespace(cnt) + "<= " + methodToString(m) + " EXCEPTION");
         }
     }
 
@@ -236,15 +243,26 @@ public class Test988 {
         }
     }
 
+    static final int METHOD_TRACING_IGNORE_DEPTH = 2;
+    static boolean sMethodTracingIgnore = false;
+
     public static void notifyMethodEntry(Object m) {
         // Called by native code when a method is entered. This method is ignored by the native
         // entry and exit hooks.
-        results.add(new MethodEntry(m, cnt));
         cnt++;
+        if ((cnt - 1) > METHOD_TRACING_IGNORE_DEPTH && sMethodTracingIgnore) {
+          return;
+        }
+        results.add(new MethodEntry(m, cnt - 1));
     }
 
     public static void notifyMethodExit(Object m, boolean exception, Object result) {
         cnt--;
+
+        if (cnt > METHOD_TRACING_IGNORE_DEPTH && sMethodTracingIgnore) {
+          return;
+        }
+
         if (exception) {
             results.add(new MethodThrownThrough(m, cnt));
         } else {
@@ -266,6 +284,10 @@ public class Test988 {
         doFibTest(5, new RecurOp());
         doFibTest(-19, new IterOp());
         doFibTest(-19, new RecurOp());
+
+        sMethodTracingIgnore = true;
+        IntrinsicsTest.doTest();
+        sMethodTracingIgnore = false;
         // Turn off method tracing so we don't have to deal with print internals.
         Trace.disableTracing(Thread.currentThread());
         printResults();
@@ -284,6 +306,7 @@ public class Test988 {
       RecurOp.class.toString();
       IterOp.class.toString();
       StringBuilder.class.toString();
+      IntrinsicsTest.initialize();  // ensure <clinit> is executed prior to tracing.
     }
 
     public static void printResults() {
@@ -298,6 +321,32 @@ public class Test988 {
         results.add(new FibResult("fibonacci(%d)=%d\n", x, y));
       } catch (Throwable t) {
         results.add(new FibThrow("fibonacci(%d) -> %s\n", x, t));
+      }
+    }
+
+    static class IntrinsicsTest {
+      static int[] sSourceArray = { 0, 1, 2, 3, 4, 5 };
+      static int[] sDestArray =   { 5, 6, 7, 8, 9, 10 };
+
+      static char[] sSourceArrayChar = { '0', '1', '2', '3', '4', '5' };
+      static char[] sDestArrayChar =   { '5', '6', '7', '8', '9', 'a' };
+
+      static void initialize() {
+        Test988Intrinsics.initialize();
+
+        // Pre-load all classes used in #doTest manual intrinsics.
+        java.lang.System.class.toString();
+      }
+      static void doTest() {
+        // Ensure that the ART intrinsics in intrinsics_list.h are also being traced,
+        // since in non-tracing operation they are effectively inlined by the optimizing compiler.
+
+        // Auto-generated test file that uses null/0s as default parameters.
+        Test988Intrinsics.test();
+
+        // Manual list here for functions that require special non-null/non-zero parameters:
+        System.arraycopy(sSourceArray, 0, sDestArray, 0, 1);
+        System.arraycopy(sSourceArrayChar, 0, sDestArrayChar, 0, 1);
       }
     }
 }

test/988-method-trace/src/art/Test988Intrinsics.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// AUTO-GENENERATED by gen_srcs.py: DO NOT EDIT HERE DIRECTLY.
+//
+// $> python3.4 gen_srcs.py > src/art/Test988Intrinsics.java
+//
+// RUN ABOVE COMMAND TO REGENERATE THIS FILE.
+
+package art;
+
+class Test988Intrinsics {
+  // Pre-initialize *all* instance variables used so that their constructors are not in the trace.
+  static java.lang.String instance_java_lang_String = "some large string";
+  static java.lang.StringBuffer instance_java_lang_StringBuffer = new java.lang.StringBuffer("some large string buffer");
+  static java.lang.StringBuilder instance_java_lang_StringBuilder = new java.lang.StringBuilder("some large string builder");
+
+  static void initialize() {
+    // Ensure all static variables are initialized.
+    // In addition, pre-load classes here so that we don't see diverging class loading traces.
+    java.lang.Double.class.toString();
+    java.lang.Float.class.toString();
+    java.lang.Integer.class.toString();
+    java.lang.Long.class.toString();
+    java.lang.Short.class.toString();
+    java.lang.Math.class.toString();
+    java.lang.Thread.class.toString();
+    java.lang.String.class.toString();
+    java.lang.StringBuffer.class.toString();
+    java.lang.StringBuilder.class.toString();
+  }
+
+  static void test() {
+    // Call each intrinsic from art/compiler/intrinsics_list.h to make sure they are traced.
+    java.lang.Double.doubleToRawLongBits(0.0);
+    java.lang.Double.doubleToLongBits(0.0);
+    java.lang.Double.isInfinite(0.0);
+    java.lang.Double.isNaN(0.0);
+    java.lang.Double.longBitsToDouble(0L);
+    java.lang.Float.floatToRawIntBits(0.0f);
+    java.lang.Float.floatToIntBits(0.0f);
+    java.lang.Float.isInfinite(0.0f);
+    java.lang.Float.isNaN(0.0f);
+    java.lang.Float.intBitsToFloat(0);
+    java.lang.Integer.reverse(0);
+    java.lang.Integer.reverseBytes(0);
+    java.lang.Integer.bitCount(0);
+    java.lang.Integer.compare(0,0);
+    java.lang.Integer.highestOneBit(0);
+    java.lang.Integer.lowestOneBit(0);
+    java.lang.Integer.numberOfLeadingZeros(0);
+    java.lang.Integer.numberOfTrailingZeros(0);
+    java.lang.Integer.rotateRight(0,0);
+    java.lang.Integer.rotateLeft(0,0);
+    java.lang.Integer.signum(0);
+    java.lang.Long.reverse(0L);
+    java.lang.Long.reverseBytes(0L);
+    java.lang.Long.bitCount(0L);
+    java.lang.Long.compare(0L,0L);
+    java.lang.Long.highestOneBit(0L);
+    java.lang.Long.lowestOneBit(0L);
+    java.lang.Long.numberOfLeadingZeros(0L);
+    java.lang.Long.numberOfTrailingZeros(0L);
+    java.lang.Long.rotateRight(0L,0);
+    java.lang.Long.rotateLeft(0L,0);
+    java.lang.Long.signum(0L);
+    java.lang.Short.reverseBytes((short)0);
+    java.lang.Math.abs(0.0);
+    java.lang.Math.abs(0.0f);
+    java.lang.Math.abs(0L);
+    java.lang.Math.abs(0);
+    java.lang.Math.min(0.0,0.0);
+    java.lang.Math.min(0.0f,0.0f);
+    java.lang.Math.min(0L,0L);
+    java.lang.Math.min(0,0);
+    java.lang.Math.max(0.0,0.0);
+    java.lang.Math.max(0.0f,0.0f);
+    java.lang.Math.max(0L,0L);
+    java.lang.Math.max(0,0);
+    java.lang.Math.cos(0.0);
+    java.lang.Math.sin(0.0);
+    java.lang.Math.acos(0.0);
+    java.lang.Math.asin(0.0);
+    java.lang.Math.atan(0.0);
+    java.lang.Math.atan2(0.0,0.0);
+    java.lang.Math.cbrt(0.0);
+    java.lang.Math.cosh(0.0);
+    java.lang.Math.exp(0.0);
+    java.lang.Math.expm1(0.0);
+    java.lang.Math.hypot(0.0,0.0);
+    java.lang.Math.log(0.0);
+    java.lang.Math.log10(0.0);
+    java.lang.Math.nextAfter(0.0,0.0);
+    java.lang.Math.sinh(0.0);
+    java.lang.Math.tan(0.0);
+    java.lang.Math.tanh(0.0);
+    java.lang.Math.sqrt(0.0);
+    java.lang.Math.ceil(0.0);
+    java.lang.Math.floor(0.0);
+    java.lang.Math.rint(0.0);
+    java.lang.Math.round(0.0);
+    java.lang.Math.round(0.0f);
+    java.lang.Thread.currentThread();
+    instance_java_lang_String.charAt(0);
+    instance_java_lang_String.compareTo("hello");
+    instance_java_lang_String.equals((java.lang.Object)null);
+    instance_java_lang_String.indexOf(0);
+    instance_java_lang_String.indexOf(0,0);
+    instance_java_lang_String.indexOf("hello");
+    instance_java_lang_String.indexOf("hello",0);
+    instance_java_lang_String.isEmpty();
+    instance_java_lang_String.length();
+    instance_java_lang_StringBuffer.append("hello");
+    instance_java_lang_StringBuffer.length();
+    instance_java_lang_StringBuffer.toString();
+    instance_java_lang_StringBuilder.append("hello");
+    instance_java_lang_StringBuilder.length();
+    instance_java_lang_StringBuilder.toString();
+    java.lang.Integer.valueOf(0);
+    java.lang.Thread.interrupted();
+  }
+}

anestisb/oatdump_plus

test/988-method-trace/src/art/Test988.java
@@ -57,7 +57,7 @@ public class Test988 {
         }
         @Override
         public void Print() {
-            System.out.println(whitespace(cnt) + "=> " + m);
+            System.out.println(whitespace(cnt) + "=> " + methodToString(m));
         }
     }
 
@@ -124,6 +124,13 @@ public class Test988 {
       }
     }
 
+    static String methodToString(Object m) {
+      // Make the output more similar between ART and RI,
+      // by removing the 'native' specifier from methods.
+      String methodStr = m.toString();
+      return methodStr.replaceFirst(" native", "");
+    }
+
     static final class MethodReturn implements Printable {
         private Object m;
         private Object val;
@@ -154,7 +161,7 @@ public class Test988 {
               klass_print = klass.toString();
             }
             System.out.println(
-                whitespace(cnt) + "<= " + m + " -> <" + klass_print + ": " + print + ">");
+                whitespace(cnt) + "<= " + methodToString(m) + " -> <" + klass_print + ": " + print + ">");
         }
     }
 
@@ -167,7 +174,7 @@ public class Test988 {
         }
         @Override
         public void Print() {
-            System.out.println(whitespace(cnt) + "<= " + m + " EXCEPTION");
+            System.out.println(whitespace(cnt) + "<= " + methodToString(m) + " EXCEPTION");
         }
     }
 
@@ -255,15 +262,26 @@ public class Test988 {
         }
     }
 
+    static final int METHOD_TRACING_IGNORE_DEPTH = 2;
+    static boolean sMethodTracingIgnore = false;
+
     public static void notifyMethodEntry(Object m) {
         // Called by native code when a method is entered. This method is ignored by the native
         // entry and exit hooks.
-        results.add(new MethodEntry(m, cnt));
         cnt++;
+        if ((cnt - 1) > METHOD_TRACING_IGNORE_DEPTH && sMethodTracingIgnore) {
+          return;
+        }
+        results.add(new MethodEntry(m, cnt - 1));
     }
 
     public static void notifyMethodExit(Object m, boolean exception, Object result) {
         cnt--;
+
+        if (cnt > METHOD_TRACING_IGNORE_DEPTH && sMethodTracingIgnore) {
+          return;
+        }
+
         if (exception) {
             results.add(new MethodThrownThrough(m, cnt));
         } else {
@@ -285,6 +303,10 @@ public class Test988 {
         doFibTest(5, new RecurOp());
         doFibTest(-19, new IterOp());
         doFibTest(-19, new RecurOp());
+
+        sMethodTracingIgnore = true;
+        IntrinsicsTest.doTest();
+        sMethodTracingIgnore = false;
         // Turn off method tracing so we don't have to deal with print internals.
         Trace.disableTracing(Thread.currentThread());
         printResults();
@@ -303,6 +325,7 @@ public class Test988 {
       RecurOp.class.toString();
       IterOp.class.toString();
       StringBuilder.class.toString();
+      IntrinsicsTest.initialize();  // ensure <clinit> is executed prior to tracing.
     }
 
     public static void printResults() {
@@ -317,6 +340,32 @@ public class Test988 {
         results.add(new FibResult("fibonacci(%d)=%d\n", x, y));
       } catch (Throwable t) {
         results.add(new FibThrow("fibonacci(%d) -> %s\n", x, t));
+      }
+    }
+
+    static class IntrinsicsTest {
+      static int[] sSourceArray = { 0, 1, 2, 3, 4, 5 };
+      static int[] sDestArray =   { 5, 6, 7, 8, 9, 10 };
+
+      static char[] sSourceArrayChar = { '0', '1', '2', '3', '4', '5' };
+      static char[] sDestArrayChar =   { '5', '6', '7', '8', '9', 'a' };
+
+      static void initialize() {
+        Test988Intrinsics.initialize();
+
+        // Pre-load all classes used in #doTest manual intrinsics.
+        java.lang.System.class.toString();
+      }
+      static void doTest() {
+        // Ensure that the ART intrinsics in intrinsics_list.h are also being traced,
+        // since in non-tracing operation they are effectively inlined by the optimizing compiler.
+
+        // Auto-generated test file that uses null/0s as default parameters.
+        Test988Intrinsics.test();
+
+        // Manual list here for functions that require special non-null/non-zero parameters:
+        System.arraycopy(sSourceArray, 0, sDestArray, 0, 1);
+        System.arraycopy(sSourceArrayChar, 0, sDestArrayChar, 0, 1);
       }
     }
 }

test/988-method-trace/src/art/Test988Intrinsics.java
@@ -1,0 +1,135 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// AUTO-GENENERATED by gen_srcs.py: DO NOT EDIT HERE DIRECTLY.
+//
+// $> python3.4 gen_srcs.py > src/art/Test988Intrinsics.java
+//
+// RUN ABOVE COMMAND TO REGENERATE THIS FILE.
+
+package art;
+
+class Test988Intrinsics {
+  // Pre-initialize *all* instance variables used so that their constructors are not in the trace.
+  static java.lang.String instance_java_lang_String = "some large string";
+  static java.lang.StringBuffer instance_java_lang_StringBuffer = new java.lang.StringBuffer("some large string buffer");
+  static java.lang.StringBuilder instance_java_lang_StringBuilder = new java.lang.StringBuilder("some large string builder");
+
+  static void initialize() {
+    // Ensure all static variables are initialized.
+    // In addition, pre-load classes here so that we don't see diverging class loading traces.
+    java.lang.Double.class.toString();
+    java.lang.Float.class.toString();
+    java.lang.Integer.class.toString();
+    java.lang.Long.class.toString();
+    java.lang.Short.class.toString();
+    java.lang.Math.class.toString();
+    java.lang.Thread.class.toString();
+    java.lang.String.class.toString();
+    java.lang.StringBuffer.class.toString();
+    java.lang.StringBuilder.class.toString();
+  }
+
+  static void test() {
+    // Call each intrinsic from art/compiler/intrinsics_list.h to make sure they are traced.
+    java.lang.Double.doubleToRawLongBits(0.0);
+    java.lang.Double.doubleToLongBits(0.0);
+    java.lang.Double.isInfinite(0.0);
+    java.lang.Double.isNaN(0.0);
+    java.lang.Double.longBitsToDouble(0L);
+    java.lang.Float.floatToRawIntBits(0.0f);
+    java.lang.Float.floatToIntBits(0.0f);
+    java.lang.Float.isInfinite(0.0f);
+    java.lang.Float.isNaN(0.0f);
+    java.lang.Float.intBitsToFloat(0);
+    java.lang.Integer.reverse(0);
+    java.lang.Integer.reverseBytes(0);
+    java.lang.Integer.bitCount(0);
+    java.lang.Integer.compare(0, 0);
+    java.lang.Integer.highestOneBit(0);
+    java.lang.Integer.lowestOneBit(0);
+    java.lang.Integer.numberOfLeadingZeros(0);
+    java.lang.Integer.numberOfTrailingZeros(0);
+    java.lang.Integer.rotateRight(0, 0);
+    java.lang.Integer.rotateLeft(0, 0);
+    java.lang.Integer.signum(0);
+    java.lang.Long.reverse(0L);
+    java.lang.Long.reverseBytes(0L);
+    java.lang.Long.bitCount(0L);
+    java.lang.Long.compare(0L, 0L);
+    java.lang.Long.highestOneBit(0L);
+    java.lang.Long.lowestOneBit(0L);
+    java.lang.Long.numberOfLeadingZeros(0L);
+    java.lang.Long.numberOfTrailingZeros(0L);
+    java.lang.Long.rotateRight(0L, 0);
+    java.lang.Long.rotateLeft(0L, 0);
+    java.lang.Long.signum(0L);
+    java.lang.Short.reverseBytes((short)0);
+    java.lang.Math.abs(0.0);
+    java.lang.Math.abs(0.0f);
+    java.lang.Math.abs(0L);
+    java.lang.Math.abs(0);
+    java.lang.Math.min(0.0, 0.0);
+    java.lang.Math.min(0.0f, 0.0f);
+    java.lang.Math.min(0L, 0L);
+    java.lang.Math.min(0, 0);
+    java.lang.Math.max(0.0, 0.0);
+    java.lang.Math.max(0.0f, 0.0f);
+    java.lang.Math.max(0L, 0L);
+    java.lang.Math.max(0, 0);
+    java.lang.Math.cos(0.0);
+    java.lang.Math.sin(0.0);
+    java.lang.Math.acos(0.0);
+    java.lang.Math.asin(0.0);
+    java.lang.Math.atan(0.0);
+    java.lang.Math.atan2(0.0, 0.0);
+    java.lang.Math.cbrt(0.0);
+    java.lang.Math.cosh(0.0);
+    java.lang.Math.exp(0.0);
+    java.lang.Math.expm1(0.0);
+    java.lang.Math.hypot(0.0, 0.0);
+    java.lang.Math.log(0.0);
+    java.lang.Math.log10(0.0);
+    java.lang.Math.nextAfter(0.0, 0.0);
+    java.lang.Math.sinh(0.0);
+    java.lang.Math.tan(0.0);
+    java.lang.Math.tanh(0.0);
+    java.lang.Math.sqrt(0.0);
+    java.lang.Math.ceil(0.0);
+    java.lang.Math.floor(0.0);
+    java.lang.Math.rint(0.0);
+    java.lang.Math.round(0.0);
+    java.lang.Math.round(0.0f);
+    java.lang.Thread.currentThread();
+    instance_java_lang_String.charAt(0);
+    instance_java_lang_String.compareTo("hello");
+    instance_java_lang_String.equals((java.lang.Object)null);
+    instance_java_lang_String.indexOf(0);
+    instance_java_lang_String.indexOf(0, 0);
+    instance_java_lang_String.indexOf("hello");
+    instance_java_lang_String.indexOf("hello", 0);
+    instance_java_lang_String.isEmpty();
+    instance_java_lang_String.length();
+    instance_java_lang_StringBuffer.append("hello");
+    instance_java_lang_StringBuffer.length();
+    instance_java_lang_StringBuffer.toString();
+    instance_java_lang_StringBuilder.append("hello");
+    instance_java_lang_StringBuilder.length();
+    instance_java_lang_StringBuilder.toString();
+    java.lang.Integer.valueOf(0);
+    java.lang.Thread.interrupted();
+  }
+}